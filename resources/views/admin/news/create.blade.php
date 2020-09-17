64569d0cb429a7c5e381d4b441f6b01a3eb5ebbc
”YoshikawaYui” <yui8811tanaka@gmail.com>
2020-09-17 05:52:50 +0000

    PHP/Laravel 13 ニュース投稿画面を作成しよう

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -13,33 +13,34 @@
       <div class="col-md-8 mx-auto">
         <h2>ニュース新規作成</h2>
         <form action="{{ action('Admin\NewsController@create') }}" method="post" enctype="multipart/form-data">
-          @if (count($errors) > 0)
-            <ul>
-              @foreach($errors->all() as $e)
-                <li>{{ $e }}</li>
-              @endforeach
-            </ul>
-          @endif
-          <div class ="form-group row">
-            <laravel class="col-md-2">タイトル</laravel>
-            <div class="col-md-10">
-              <input type="text" class="form-control" name="title" value="{{ old( 'title') }}">
+
+            @if (count($errors) > 0)
+                <ul>
+                    @foreach($errors->all() as $e)
+                        <li>{{ $e }}</li>
+                    @endforeach
+                </ul>
+            @endif
+            <div class="form-group row">
+                <label class="col-md-2">タイトル</label>
+                <div class="col-md-10">
+                    <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                </div>
             </div>
-          </div>
-          <div class ="form-group row">
-            <laravel class="col-md-2">本文</laravel>
-            <div class="col-md-10">
-              <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+            <div class="form-group row">
+                <label class="col-md-2">本文</label>
+                <div class="col-md-10">
+                    <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+                </div>
             </div>
-          </div>
-          <div class ="form-group row">
-            <laravel class="col-md-10">画像</laravel>
-            <div class="col-md-10">
-              <input type="file" class="form-control-file" name="image">
+            <div class="form-group row">
+                <label class="col-md-2">画像</label>
+                <div class="col-md-10">
+                    <input type="file" class="form-control-file" name="image">
+                </div>
             </div>
-          </div>
-          {{ csrf_field() }}
-          <input type ="submit" class="btn btn-primary" value="更新">
+            {{ csrf_field() }}
+            <input type="submit" class="btn btn-primary" value="更新">
         </form>
       </div>
     </div>
