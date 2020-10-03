737336b56deb96435d7b236b6d707176328cbaa9
”YoshikawaYui” <yui8811tanaka@gmail.com>
2020-10-03 07:13:05 +0000

    heroku修正

--- app/Profile.php
+++ app/Profile.php
@@ -7,12 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     //
-  protected $guarded = array('id');
+    protected $guarded = array('id');
   
-  public static $rules = array(
-    'name' => 'required',
-    'gender' => 'required',
-    'hobby' => 'required',
-    'introduction' => 'required',
-  );
+    public static $rules = array(
+      'name' => 'required',
+      'gender' => 'required',
+      'hobby' => 'required',
+      'introduction' => 'required',
+    );
+  
+    public function histories()
+    {
+        return $this->hasMany('App\ProfileHistory');
+    }
 }
